9d2e2b0bc0810fca2570aa992872ef89a5a7031a
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2024-11-24 20:52:39 +0100

    Add fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
 
-// Do not edit below this line
+    return array.filter(checkedElement => !args.includes(checkedElement));          // .filter creates a new array, iterating on every element of the original array and checking a condition
+                                                                                    // through a function to which it passes the element as first argument.
+                                                                                    // If said condition is true (if the function returns true) it adds that element to the new array.
+};                                                                                  // ARRAY.includes(VALUE) returns true when VALUE is inside ARRAY.
+                                                                                    // So in this case if the checkedElement is included in the arguments (args), the function inside filter
+// Do not edit below this line                                                      // returns false and the element is removed.
 module.exports = removeFromArray;
