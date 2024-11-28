0e3cc11bfbc4fe65ae245f6448e27ae550187b59
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2024-11-28 12:18:00 +0100

    Complete exercises 08, 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, addend) => total + addend, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, multiplier) =>  product*multiplier, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  for(let i=0; i<(a-1); i++){
+    result = result*(a-i);
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const array = str.toLowerCase().split("");
+    const filteredArray = array.filter(isLetterOrNumber);
+    const invertedArray = filteredArray.slice().reverse(); //reverse modificherebbe l'array originale, per questo entra in gioco slice
+    if((filteredArray.length == invertedArray.length) && (invertedArray.every((value, index)=>(value === filteredArray[index])))){
+        return true;
+    }else {return false};
+};
 
+const isLetterOrNumber = function (char) {
+    const code = char.charCodeAt(0);
+    // Check for:
+    // - Lowercase letters (a-z)
+    // - Uppercase letters (A-Z)
+    // - Digits (0-9)
+    return (
+        (code >= 48 && code <= 57) || // Numbers (0-9)
+        (code >= 65 && code <= 90) || // Uppercase letters (A-Z)
+        (code >= 97 && code <= 122)   // Lowercase letters (a-z)
+    );
 };
 
 // Do not edit below this line
